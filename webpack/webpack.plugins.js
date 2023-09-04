c3cb4a97075f7e280935ef2ec5410eb410c20fd8
Petikov Vladimir (KROX) <developer-krox@yandex.ru>
2023-09-04 18:28:08 +0300

    040923-1830 Начало работы над проектом

---
+++ webpack/webpack.plugins.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const StatoscopeWebpackPlugin = require('@statoscope/webpack-plugin').default;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin')
+const Dotenv = require('dotenv-webpack');
+const mode = process.env.NODE_ENV;
+
+module.exports = () => {
+    return {
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: "Cinema",
+                template: path.join(__dirname, '../src/', 'index.html'),
+                minify: (mode === 'development') ? false : true,
+                // favicon: path.join(__dirname, "src", "favicon.ico"),
+            }),
+            new MiniCssExtractPlugin({
+                filename: (mode === 'development') ? '[name].css' : '[name].[contenthash].css',
+            }),
+            new Dotenv({
+                path: './.env.local',
+            }),
+            new StatoscopeWebpackPlugin(),
+        ]
+    }
+}
